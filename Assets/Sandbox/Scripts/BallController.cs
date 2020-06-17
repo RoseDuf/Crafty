cbdc2dc31d1942e25657ec5f86353a21f04a56bb
RoseDuf <rose.dufresne@outlook.com>
2020-06-17 16:49:25 -0400

    got good 3rd person camera view + starting the BallConstoller inputs

---
+++ Assets/Sandbox/Scripts/BallController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.BallController
+{
+
+    public class BallController : MonoBehaviour
+    {
+
+        [SerializeField] float hit_power;
+
+        Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+            HandleInput();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void HandleInput()
+        {
+            if (Input.GetButtonDown("Lob"))
+            {
+                print("hit");
+                rb.AddForce(hit_power, hit_power, hit_power, ForceMode.Impulse);
+            }
+            if (Input.GetButtonDown("Straight"))
+            {
+
+            }
+        }
+    }
+}
---
+++ Assets/Sandbox/Scripts/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] GameObject cameraObject;
+
+    [SerializeField] GameObject ballFocus;
+    [SerializeField] GameObject projectionFocus;
+
+    [SerializeField] float cameraMoveSpeed;
+    [SerializeField] float clampAngle; //how high/low we want to be able to look
+    [SerializeField] float mouseSensitivity; 
+
+    private Vector3 followPosition;
+    private Vector3 followRotation;
+
+    private float finalInputX;
+    private float finalInputZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        followPosition = transform.position;
+        followRotation = transform.localRotation.eulerAngles;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        followRotation.y += mouseX * mouseSensitivity * Time.deltaTime;
+        followRotation.x += mouseY * mouseSensitivity * Time.deltaTime;
+
+        followRotation.x = Mathf.Clamp(followRotation.x, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(followRotation.x, followRotation.y, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Transform target = ballFocus.transform;
+
+        float step = cameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
