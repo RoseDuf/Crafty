4146857526bb281046a7cc96369dc5c8852ee457
Daniel <danieldavidrinaldi@gmail.com>
2020-06-30 18:43:14 -0400

    Cleaned up after myself
    - Removed some commented out trashed code.

--- Assets/_Scripts/Scenes/LevelScene.cs
+++ Assets/_Scripts/Scenes/LevelScene.cs
@@ -16,11 +16,8 @@ namespace Game.Scenes
         private bool paused;
         private List<Target> targets;
 
-        private Dictionary<GameObject, bool> pausedObjects;
-
         private void Awake()
         {
-            pausedObjects = new Dictionary<GameObject, bool>();
             paused = false;
             targets = new List<Target>();
 
@@ -39,12 +36,6 @@ namespace Game.Scenes
                 paused = true;
                 pauseMenu?.gameObject.SetActive(true);
                 Time.timeScale = 0;
-                
-                //if (pauseMenu != null)
-                //{
-                //    pauseMenu?.gameObject.SetActive(true);
-                //    Paused = true;
-                //}
             }
             else if (Input.GetKeyDown(KeyCode.Escape) && paused)
             {
@@ -63,64 +54,9 @@ namespace Game.Scenes
                 targets[0].SpawnHole();
         }
 
-        //public bool Paused
-        //{
-        //    get { return paused; }
-        //    set 
-        //    {
-        //        if (paused != value)
-        //        {
-        //            paused = value;
-        //            if (paused)
-        //            {
-        //                StoreStateOfPausedObjects();
-
-        //                StartCoroutine(CRPause());
-        //            }
-        //        }
-        //    }
-        //}
-
-        //private void LateUpdate()
-        //{
-        //    if (paused)
-        //    {
-        //        foreach (GameObject go in pausedObjects.Keys)
-        //            go.SetActive(pausedObjects[go]);
-        //    }
-        //}
-
-        //private void StoreStateOfPausedObjects()
-        //{
-        //    pausedObjects.Clear();
-        //    for (int i = 0; i < this.transform.root.childCount; i++)
-        //    {
-        //        GameObject rootGameObjectChild = this.transform.root.GetChild(i).gameObject;
-        //        if (!rootGameObjectChild.name.Equals("UI") &&
-        //            !rootGameObjectChild.name.Equals("Lights") && !pausedObjects.ContainsKey(rootGameObjectChild))
-        //            pausedObjects.Add(rootGameObjectChild, rootGameObjectChild.activeSelf);
-        //    }
-
-        //    for (int i = 0; i < pauseMenu.transform.parent.childCount; i++)
-        //    {
-        //        GameObject uiGameObjectChild = pauseMenu.transform.parent.GetChild(i).gameObject;
-        //        if (!uiGameObjectChild.Equals(pauseMenu.gameObject) && !pausedObjects.ContainsKey(uiGameObjectChild))
-        //            pausedObjects.Add(uiGameObjectChild, uiGameObjectChild.activeSelf);
-        //    }
-        //}
-
-        //private IEnumerator CRPause()
-        //{
-        //    while (paused)
-        //    {
-        //        yield return new WaitForEndOfFrame();
-
-        //        foreach (GameObject go in pausedObjects.Keys)
-        //            go.SetActive(false);
-        //    }
-
-        //    foreach (GameObject go in pausedObjects.Keys)
-        //        go.SetActive(pausedObjects[go]);
-        //}
+        private void OnDestroy()
+        {
+            Time.timeScale = 1;
+        }
     }
 }
